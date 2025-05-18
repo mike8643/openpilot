57a009283d79b3191add8b1cbade314946866179
mike8643 <nguyen95465@yahoo.com>
2025-05-17 20:27:00 -0400

    cruise_standstill

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -12,6 +12,7 @@ LongCtrlState = car.CarControl.Actuators.LongControlState
 
 def long_control_state_trans(CP, active, long_control_state, v_ego,
                              should_stop, brake_pressed, cruise_standstill):
+  cruise_standstill = cruise_standstill and not CP.enableGasInterceptor
   stopping_condition = should_stop
   starting_condition = (not should_stop and
                         not cruise_standstill and
